013b4f5df5782f6bd86f22c0106072ef64586988
jengku <jengku@gmail.com>
2017-07-20 09:12:32 +0800

    utils.h

---
+++ example_main.cpp
@@ -0,0 +1,5 @@
+#include "util.h"
+
+int main(void) {
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef _UTILS_HEADER
+#define _UTILS_HEADER
+#include "Image/Image.h"
+namespace Lodepng
+{
+#include "lodepng/lodepng.h"
+};
+
+#endif
