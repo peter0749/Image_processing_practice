ad096d30c64485fd5bb1d510acf8511710846cbf
jengku <jengku@gmail.com>
2017-07-23 16:09:23 +0800

    foo

--- ZJb445/TextureTransfer.cpp
+++ ZJb445/TextureTransfer.cpp
@@ -2,7 +2,6 @@
 #define __INCLUDE_TEXTURE_TRANSFER_CPP
 #include <iostream>
 #include "TextureTransfer.h"
-
 namespace ImageProcess
 {
     #define pixSum(_X) ((int)_X.r + (int)_X.g + (int)_X.b + (int)_X.alpha)
@@ -74,76 +73,16 @@ namespace ImageProcess
         #define of(_X,_Y) ((_X)*(overlap+2)+(_Y))
         int Width = min((int)pattSize, (int)srcImg.img_W-sx);
         int Height= min((int)pattSize, (int)srcImg.img_H-sy);
-        size_t dp1Size = Width*(overlap+2);
-        size_t dp2Size = Height*(overlap+2);
-        int *dp1 = new int[dp1Size];
-        int *dp2 = new int[dp2Size];
-        fill(dp1, dp1+dp1Size, 1e9);
-        fill(dp2, dp2+dp2Size, 1e9);
-        for (int i=0; i<overlap; ++i) {
-            Pix rp1(result.getPixel(sx, sy+i));
-            Pix rp2(result.getPixel(sx+i, sy));
-            Pix tp1(texture.getPixel(x, y+i));
-            Pix tp2(texture.getPixel(x+i, y));
-            dp1[i+1] = diff(rp1, tp1);
-            dp2[i+1] = diff(rp2, tp2);
-        }
-        #define LAXY(XY, ___INLINE___) for (int i=1; i<XY; ++i) for (int j=1; j<=overlap; ++j) {  int minval=1e9; ___INLINE___  }
-        LAXY( Width, for (int k=-1; k<=1; ++k)
-                minval=min(minval, dp1[of(i-1,j+k)]);
-                Pix r(result.getPixel(sx+i, sy+j-1));
-                Pix t(texture.getPixel(x+i, y+j-1));
-                dp1[of(i,j)] = minval + diff(r,t);
-            )
-        LAXY( Height,for (int k=-1; k<=1; ++k) 
-                minval=min(minval, dp2[of(i-1,j+k)]);
-                Pix r(result.getPixel(sx+j-1, sy+i));
-                Pix t(texture.getPixel(x+j-1,  y+i));
-                dp2[of(i,j)] = minval + diff(r,t);
-            )
-        #undef LAXY
-        #define LAXY(___INLINE___) \
+        size_t dp1Size, dp2Size;
+        int *dp1, *dp2;
+        #define LAZY1(XY, ___INLINE___) for (int i=1; i<XY; ++i) for (int j=1; j<=overlap; ++j) {  int minval=1e9; ___INLINE___  }
+        #define LAZY2(___INLINE___) \
         { \
             int mindp=1; \
             int minv =1e9; \
             for (int i=1; i<=overlap; ++i) \
                 ___INLINE___ \
         }
-        LAXY(if (dp1[of(Width-1,i)]<minv) {
-                minv = dp1[of(Width-1,i)];
-                mindp= i;
-             }
-             for (int i=Width-1; i>=1; --i) {
-                Pix r(result.getPixel(sx+i, sy+mindp-1));
-                Pix t(texture.getPixel(x+i, y+mindp-1));
-                int val = diff(r,t);
-                for (int j=-1; j<=1; ++j) {
-                    if (dp1[of(i-1, mindp+j)]+val==dp1[of(i,mindp)]) {
-                        dp1[of(i, mindp)] = INT_MIN;
-                        mindp += j; break;
-                    }
-                }
-             }
-             dp1[of(0,mindp)]=INT_MIN;
-            )
-        LAXY(if (dp2[of(Height-1,i)]<minv) {
-                minv = dp2[of(Height-1,i)];
-                mindp= i;
-             }
-             for (int i=Height-1; i>=1; --i) {
-                Pix r(result.getPixel(sx+mindp-1, sy+i));
-                Pix t(texture.getPixel(x+mindp-1, y+i));
-                int val = diff(r,t);
-                for (int j=-1; j<=1; ++j) {
-                    if (dp2[of(i-1, mindp+j)]+val==dp2[of(i,mindp)]) {
-                        dp2[of(i, mindp)] = INT_MIN;
-                        mindp += j; break;
-                    }
-                }
-             }
-             dp2[of(0,mindp)]=INT_MIN;
-            )
-        #undef LAXY
         Image *temp = new Image(Width, Height);
         for (int i=0; i<Height; ++i) {
             for (int j=0; j<Width; ++j) {
@@ -151,24 +90,94 @@ namespace ImageProcess
                 result.setPixel(sx+j,sy+i, texture.getPixel(x+j, y+i));
             }
         }
-        for (int i=0; i<Width; ++i) {
-            for (int j=0; j<Height; ++j) {
-                if (dp1[of(i, j+1)]==INT_MIN) break;
-                result.setPixel(sx+i,sy+j, temp->getPixel(i,j));
+        if (sy!=0) {
+            dp1Size = Width*(overlap+2);
+            dp1 = new int[dp1Size];
+            fill(dp1, dp1+dp1Size, 1e9);
+            for (int i=0; i<overlap; ++i) {
+                Pix rp1(result.getPixel(sx, sy+i));
+                Pix tp1(texture.getPixel(x, y+i));
+                dp1[i+1] = diff(rp1, tp1);
             }
+            LAZY1( Width, 
+                    for (int k=-1; k<=1; ++k)
+                        minval=min(minval, dp1[of(i-1,j+k)]);
+                    Pix r(result.getPixel(sx+i, sy+j-1));
+                    Pix t(texture.getPixel(x+i, y+j-1));
+                    dp1[of(i,j)] = minval + diff(r,t);
+                 )
+            LAZY2(if (dp1[of(Width-1,i)]<minv) {
+                      minv = dp1[of(Width-1,i)];
+                      mindp= i;
+                  }
+                  for (int i=Width-1; i>=1; --i) {
+                      Pix r(result.getPixel(sx+i, sy+mindp-1));
+                      Pix t(texture.getPixel(x+i, y+mindp-1));
+                      int val = diff(r,t);
+                      for (int j=-1; j<=1; ++j) {
+                          if (dp1[of(i-1, mindp+j)]+val==dp1[of(i,mindp)]) {
+                              dp1[of(i, mindp)] = INT_MIN;
+                              mindp += j; break;
+                          }
+                      }
+                  }
+                  dp1[of(0,mindp)]=INT_MIN;
+                 )
+            for (int i=0; i<Width; ++i) {
+                for (int j=0; j<Height; ++j) {
+                    if (dp1[of(i, j+1)]==INT_MIN) break;
+                    result.setPixel(sx+i,sy+j, temp->getPixel(i,j));
+                }
+            }
+            delete[] dp1;
         }
-        for (int i=0; i<Height; ++i) {
-            for (int j=0; j<Width; ++j) {
-                if (dp2[of(i, j+1)]==INT_MIN) break;
-                result.setPixel(sx+j,sy+i, temp->getPixel(j,i));
+        if (sx!=0) {
+            dp2Size = Height*(overlap+2);
+            dp2 = new int[dp2Size];
+            fill(dp2, dp2+dp2Size, 1e9);
+            for (int i=0; i<overlap; ++i) {
+                Pix rp2(result.getPixel(sx+i, sy));
+                Pix tp2(texture.getPixel(x+i, y));
+                dp2[i+1] = diff(rp2, tp2);
             }
+            LAZY1( Height,
+                    for (int k=-1; k<=1; ++k) 
+                        minval=min(minval, dp2[of(i-1,j+k)]);
+                    Pix r(result.getPixel(sx+j-1, sy+i));
+                    Pix t(texture.getPixel(x+j-1,  y+i));
+                    dp2[of(i,j)] = minval + diff(r,t);
+                 )
+            LAZY2(if (dp2[of(Height-1,i)]<minv) {
+                      minv = dp2[of(Height-1,i)];
+                      mindp= i;
+                  }
+                  for (int i=Height-1; i>=1; --i) {
+                      Pix r(result.getPixel(sx+mindp-1, sy+i));
+                      Pix t(texture.getPixel(x+mindp-1, y+i));
+                      int val = diff(r,t);
+                      for (int j=-1; j<=1; ++j) {
+                          if (dp2[of(i-1, mindp+j)]+val==dp2[of(i,mindp)]) {
+                              dp2[of(i, mindp)] = INT_MIN;
+                              mindp += j; break;
+                          }
+                      }
+                  }
+                  dp2[of(0,mindp)]=INT_MIN;
+                 )
+            for (int i=0; i<Height; ++i) {
+                for (int j=0; j<Width; ++j) {
+                    if (dp2[of(i, j+1)]==INT_MIN) break;
+                    result.setPixel(sx+j,sy+i, temp->getPixel(j,i));
+                }
+            }
+            delete[] dp2;
         }
         
+        #undef LAZY1
+        #undef LAZY2
         #undef of
         #undef diff
         delete temp;
-        delete[] dp1;
-        delete[] dp2;
     }
     void TextureTransfer::montage(void) {
         int step = pattSize-overlap;
