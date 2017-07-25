f3cfbc0b0b5d72474ecc570055d516a6af544c09
jengku <jengku@gmail.com>
2017-07-26 01:50:09 +0800

    apply a simple gaussian filter

--- ZJb452_CoinCounting/Hough.cpp
+++ ZJb452_CoinCounting/Hough.cpp
@@ -9,6 +9,29 @@ namespace ImageProcess
     HoughCircle::HoughCircle(const Image &ref, double r, double e, int th) : target(new Image(ref)), radius(r), eps(e), thres(th), accumulator(new int[ref.img_W*ref.img_H]), counter(0) {
         using std::round;
         using std::memset;
+        using std::min;
+        const int filter[3][3] = {
+            1, 2, 1,
+            2, 4, 2,
+            1, 2, 1
+        };
+        for (int i=1; i<target->img_H-1; ++i) { // not process on margin, 3x3 gaussian filter
+            for (int j=1; j<target->img_W-1; ++j) {
+                int sumR=0, sumG=0, sumB=0;
+                int alpha=target->getPixel(j,i).alpha;
+                for (int k=-1; k<=1; ++k) {
+                    for (int l=-1; l<=1; ++l) {
+                        sumR += (int)target->getPixel(j+l,i+k).r*filter[k+1][l+1];
+                        sumG += (int)target->getPixel(j+l,i+k).g*filter[k+1][l+1];
+                        sumB += (int)target->getPixel(j+l,i+k).b*filter[k+1][l+1];
+                    }
+                }
+                sumR = min(255,(int)round(sumR/16.0));
+                sumG = min(255,(int)round(sumG/16.0));
+                sumB = min(255,(int)round(sumB/16.0));
+                target->setPixel(j,i,Pix(sumR,sumG,sumB,alpha));
+            }
+        }
         Threshold blackNWhite(*target);
         Image *temp = blackNWhite.run(); // auto-thresholding
         delete target; target = temp;
